2d0bc5c13f8ceb80b73e0001c978bcbbc898af76
Charles <zwl19920514@163.com>
2015-05-19 16:30:18 +0800

    Initial Commit

---
+++ CzWeico/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CzWeico
+//
+//  Created by Charles on 15/5/19.
+//  Copyright (c) 2015年 Charles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
